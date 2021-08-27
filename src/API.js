6a6b618e076b8332db35369f7d891edbbcb40b8d
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-08-27 17:06:39 +0530

    Edited src/API.js and made necessary functions to fetch data from the API. used these functions in the Home.js in another function and used useState and useEffect hooks to try it out and it worked fine. These hooks will be replaced with a custom hook, later.

--- src/API.js
+++ src/API.js
@@ -76,3 +76,41 @@ const apiSettings = {
 
 export default apiSettings;
 */
+
+import {
+  SEARCH_BASE_URL,
+  POPULAR_BASE_URL,
+  API_KEY,
+  API_URL,
+  REQUEST_TOKEN_URL,
+  LOGIN_URL,
+  SESSION_ID_URL
+} from './config';
+
+const defaultConfig = {
+  method: 'POST',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+};
+
+export default {
+
+  fetchMovies: async (searchTerm, page) => {
+    const endpoint = searchTerm
+    ? `${SEARCH_BASE_URL}${searchTerm}&page=${page}`
+    : `${POPULAR_BASE_URL}$page=${page}`;
+    return await (await fetch(endpoint)).json();
+  },
+
+  fetchMovie: async (movieId) => {
+    const endpoint = `${API_URL}movie/${movieId}?api_key=${API_KEY}`;
+    return await (await fetch(endpoint)).json();
+  },
+
+  fetchCredits: async (movieId) => {
+    const endpoint = `${API_URL}movie/${movieId}/credits?api_key=${API_KEY}`;
+    return await (await fetch(endpoint)).json();
+  }
+  
+}
