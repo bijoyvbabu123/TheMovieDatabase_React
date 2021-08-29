8af63b2e701cd50ce677eb641b8b06f79b6392d0
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-08-29 22:13:47 +0530

    created componet SearchBar skeleton, as a controlled component, with needed timer and hooks and added it to the App.

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,6 +6,7 @@ import HeroImage from './HeroImage';
 import Grid from './Grid'
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 //Hook
 import { useHomeFetch } from '../hooks/useHomeFetch';
 //image
@@ -14,7 +15,7 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
 
-    const {state, loading, error } = useHomeFetch();
+    const {state, loading, error, setSearchTerm } = useHomeFetch();
 
     console.log(state);
 
@@ -30,6 +31,8 @@ const Home = () => {
         />)
         : null
         }
+        
+        <SearchBar setSearchTerm={setSearchTerm} />
 
         <Grid header="Popular Movies">
             {state.results.map(movie => (
