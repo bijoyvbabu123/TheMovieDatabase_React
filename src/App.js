affc96c323bf62463922c05988f7a966d186fe93
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-03 01:04:42 +0530

    set up the basic routing paths

--- src/App.js
+++ src/App.js
@@ -3,15 +3,25 @@ import React from 'react';
 import { GlobalStyle } from './GlobalStyle'
 //components
 import Header from './components/Header';
-import Home from './components/Home'
+import Home from './components/Home';
+import Movie from './components/Movie';
+import NotFound from './components/NotFound';
+//Routing
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
+    <Router>
       <Header />
-      <Home />
+      <Routes>
+
+        <Route path='/' element={<Home />} />
+        <Route path='/:movieId' element={<Movie />} />
+        <Route path='/*' element={<NotFound />} />
+
+      </Routes>
       <GlobalStyle />
-    </div>
+    </Router>
   );
 }
 
