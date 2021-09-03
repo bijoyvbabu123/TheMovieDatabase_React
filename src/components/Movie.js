3d3042fdc515f24e9f1f7195fe3cedec220f2779
bijoyvbabu123 <73835543+bijoyvbabu123@users.noreply.github.com>
2021-09-03 10:10:03 +0530

    created a custom hook to fetch movie detail to the movie detail page

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,7 +1,31 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
+// config
+import { IMG_BASE_URL, POSTER_SIZE } from '../config';
+// components
+import Grid from './Grid';
+import Spinner from './Spinner';
+// image
+import NoImage from '../images/no_image.jpg';
+// hooks
+import { useMovieFetch } from '../hooks/useMovieFetch';
 
 
 
-const Movie = () => <div>Movie</div>
+const Movie = () => {
+
+    const { movieId } = useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+
+    console.log(movie);
+
+    return (
+        <>
+            <div> Movie page </div>
+        </>
+    )
+
+}
 
 export default Movie;
---
+++ src/hooks/useMovieFetch.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from 'react';
+import API from '../API';
+
+export const useMovieFetch = (movieId) => {
+
+    const [state, setState] = useState({});
+    const [error, setError] = useState(false);
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+
+        const fetchMovie = async () => {
+            try {
+
+                setLoading(true);
+                setError(false);
+
+                const movie = await API.fetchMovie(movieId);
+                const credits = await API.fetchCredits(movieId);
+
+                // get directors
+                const directors = credits.crew.filter(
+                    member => member.job == 'Director'
+                );
+
+                setState({
+                    ...movie,
+                    actors: credits.cast,
+                    directors
+                })
+
+                setLoading(false);
+
+            } catch (error) {
+                setError(true);
+            }
+        };
+
+        fetchMovie();
+
+    }, [movieId])
+
+    return { state, loading, error };
+
+}
